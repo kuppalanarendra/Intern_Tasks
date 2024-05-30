43c23d010a5e0d06eb56232838f793703c8424b7
kuppalanarendra <kuppalanarendra123@gmail.com>
2024-05-30 17:00:28 +0530

    Added java code

---
+++ Task2/src/com/mini_library/TeacherMember.java
@@ -0,0 +1,7 @@
+package com.mini_library;
+
+public class TeacherMember extends Member {
+    public TeacherMember(int memberID, String name,int maxBooksIssued) {
+        super(memberID, name, maxBooksIssued); 
+    }
+}
