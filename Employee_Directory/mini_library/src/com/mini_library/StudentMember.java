e281df3674a4e4abdf1e4830f5f443e21b3d9e67
kuppalanarendra <150499669+kuppalanarendra@users.noreply.github.com>
2024-07-02 12:04:14 +0530

    Delete Employee_Directory directory

--- Employee_Directory/mini_library/src/com/mini_library/StudentMember.java
+++
@@ -1,9 +0,0 @@
-package com.mini_library;
-
-//Student member class which extends member class 
-//Inheritence
-public class StudentMember extends Member {
-    public StudentMember(int memberID, String name,int maxBooksIssued) {
-        super(memberID, name, maxBooksIssued);   
-    }
-}
